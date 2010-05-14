d939f265639e61bfad823d12c46d87858a39eaea
Derek Greentree <derek@lijit.com>
2010-05-13 18:14:35 -0600

    Cleaner; feels more idiomatic.

--- 24/euler24.clj
+++ 24/euler24.clj
@@ -1,38 +1,36 @@
-(ns euler.problem24)
+(ns euler24)
 
 (defn permute [coll]
-  "Returns a lazy sequence of cons cells. Each cell's car is the head of the list of the 
-  permutation it represents. Each cell's cdr is another lazy sequence representing possible
-  permutations with that prefix, and so on."
+"Returns a lazy sequence of the permutations of coll as cons cells;
+each car is the item in the permutation, and each cdr is a sequence
+of the tails of the permutations starting with that element. If coll is in
+lexicographical order, so are the permutations produced by walking the result of
+permute depth-first."
   (when-let [s (seq coll)]
-    ;; each element of coll will become the head of a new subsequence.
-    (map (fn [x] (cons x (permute (filter #(not= x %) s)))) s)))
+    (map (fn [x] (cons x (permute (remove #(= x %) s)))) s)))
 
 (defn factorial [n]
+"Returns n! for positive integer n."
   (loop [cnt n acc 1]
     (if (<= cnt 0)
       acc
       (recur (dec cnt) (* acc cnt)))))
 
-(defn- branch [s parts i acc]
-  "Seeks to the permutation at index i in permutation sequence s, representing partitions of 
-  parts remaining elements. Accumulates sequence values in acc until desired index is reached."
-  (let [partsize (factorial (dec parts))]
-    (cond
-      (< i partsize) 
-      (let [permutation (first s)
-            data (first permutation)
-            remainder (rest permutation)]
-        (if (nil? data) acc
-          (recur remainder (dec parts) i (conj acc data))))
-      :else
-        (let [permutation (nth s (quot i partsize))
-              data (first permutation)
-              remainder (rest permutation)]
-          (recur remainder (dec parts) (mod i partsize) (conj acc data))))))
+(defn- seek
+  ([s parts i acc]
+  "Seeks to the permutation at index i in permutation sequence s, representing partitions of
+parts remaining elements. Accumulates sequence values in acc until desired index is reached."
+   (if (empty? s) acc
+     (let [partsize (factorial (dec parts))
+           permutation (nth s (quot i partsize))
+           data (first permutation)
+           remainder (rest permutation)]
+       (recur remainder (dec parts) (mod i partsize) (conj acc data)))))
+  ([s parts i] (seek s parts i [])))
 
-(defn nth-permutation [coll n]
-  "Generate the nth lexicographical permutation of the items in collection coll, assuming
-  items given occur in lexicographically increasing order in the collection."
-  (branch (permute coll) (count coll) (dec n) []))
+(defn ith-permutation [n i]
+  "Generate the ith lexicographical permutation of the numbers from 0..n (inclusive)."
+  (let [coll (range (inc n))]
+    (seek (permute coll) (count coll) (dec i))))
 
+(println (ith-permutation 9 1000000))
