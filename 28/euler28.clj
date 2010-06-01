ce0aafdae0d8b0216f30b06c90ec2e8d67ec8570
Derek Greentree <derek@lijit.com>
2010-05-31 20:58:36 -0600

    Problem 28.

---
+++ 28/euler28.clj
@@ -0,0 +1,21 @@
+(ns euler28) 
+
+(let [two (BigDecimal. 2)]
+  (defn exp [n] 
+    (.pow two n)))
+
+(defn msqr [n]
+  (let [t (inc (* 2 (dec n)))]
+    (* t t)))
+
+(defn diag [n]
+  (if (<= n 1) (cons 1 nil)
+    (let [sofar (diag (dec n))
+          step (* 2 (dec n))
+          start (+ (msqr (dec n)) step)]
+      (reduce (fn [l i] (cons i l)) sofar (range (+ start) (+ (* 4 step) start) step)))))
+
+(defn sum [length]
+  (reduce + (diag (int (inc (/ length 2))))))
+
+(defn euler27 [] (sum 1001))
